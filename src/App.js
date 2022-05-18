45813842c6bb429521e12cc5bd7b91d25254d1af
Jorge Andrés Choque <jorge.ch.999santos@gmail.com>
2022-05-18 06:05:32 -0300

    Formulario terminado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import Formulario from './components/Formulario';
+import Header from './components/Header';
 
 function App() {
+
+  // definir state de la categoria y las noticias
+  const [ categoria, guardarCategoria ] = useState('');
   return (
-    <h1>Configuracion Inicial</h1>
+    <Fragment>
+      <Header 
+        titulo='Buscador de Noticias'
+      />
+      <div className='container white'>
+        <Formulario 
+            guardarCategoria={ guardarCategoria }
+        />
+      </div>
+    </Fragment>    
   );
 }
 
